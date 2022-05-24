676517ce57c64d30b91896db8cc6846c596bb912
BirdPersonFromDorm <rmelinevskiy@gmail.com>
2022-05-24 20:37:54 +0300

    first commit

---
+++ components/FullInfo.js
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React, {useState} from "react";
+import {gStyle} from "../styles/style";
+import {Button} from "react-native-web";
+
+const Contacts = () => {
+    return (
+        <View style={gStyle.main}>
+            <Text style={gStyle.title}>text</Text>
+        </View>
+    );
+}
+
+export default Contacts;
